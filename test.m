c1e410233f784a2f3f802ce780cc215962eb0950
Philip Cho <hcho3@pine.(none)>
2013-04-22 02:15:59 -0400

    Really removed test.m from version control

--- test.m
+++
@@ -1,7 +0,0 @@
-for i=5:5
-    fprintf(1,    'Processing nCPM%d.jpg ...\n', i);
-    bootstrap_gpu(sprintf('nCPM%d', i), 10, 50);
-%   fprintf(1,    'Processing nCPM%d.jpg ...\n', i);
-%   bootstrap_gpu(sprintf('nCPM%d', i), 10, 50);
-end
-%system('python notify.py');
